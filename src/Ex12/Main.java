f2d21010cec58f7a73bbf712f4382686faf679f2
LeniceFonseca <lfonseca@uta.cv>
2023-04-20 23:57:45 -0100

    fim

--- src/Ex12/Main.java
+++ src/Ex12/Main.java
@@ -7,7 +7,6 @@ public class Main {
 
     public static void main(String[] args) {
 
-        // Criação de tarefas
         TarefaUnica tarefaUnica = new TarefaUnica("Comprar pão", LocalDate.of(2023, 4, 25));
         TarefaDiaria tarefaDiaria = new TarefaDiaria("Fazer exercícios", LocalDate.of(2023, 4, 20));
         TarefaMensal tarefaMensal = new TarefaMensal("Pagar contas", LocalDate.of(2023, 4, 10), 10);
